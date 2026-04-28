0471862f7cbf35c87a81682f4ef413f2471811a2
Simarpreet Singh <harkiratsaini47@gmail.com>
2026-04-29 00:23:36 +0530

    Update axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://192.168.1.7:3000/api', // Backend server port with /api prefix
+  baseURL: import.meta.env.VITE_API_URL
   headers: {
     'Content-Type': 'application/json',
   },
